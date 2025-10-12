807f1ddc2a1a94231e9cc2ccea089979eac80937
Ahmed Nabil <ahmed.nabil.khairy.10@gmail.com>
2025-10-12 10:34:13 +0300

    Initialize database and run migrations on app startup

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
+from helpers.config import get_settings
 from routes import base_router
+from models.init_db import create_database, run_migrations
 
 app = FastAPI()
+
+async def startup_span():
+    
+    settings = get_settings()
+        
+    create_database(settings.SERVER_URL)
+    run_migrations()
+    
+async def shutdown_span():
+    pass
+
+app.on_event("startup")(startup_span)
+app.on_event("shutdown")(shutdown_span)
 
 app.include_router(base_router)

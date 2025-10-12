e5f5bd9d85b8079129c9152f7b3e11627fb2491f
Ahmed Nabil <ahmed.nabil.khairy.10@gmail.com>
2025-10-12 10:28:32 +0300

    Add DATABASE_URL and SERVER_URL to config

--- helpers/config.py
+++ helpers/config.py
@@ -3,17 +3,32 @@ from functools import lru_cache
 
 class Settings(BaseSettings):
     
-    """Database configuration"""
+    # Database configuration
     POSTGRES_USERNAME: str
     POSTGRES_PASSWORD: str
     POSTGRES_HOST: str
     POSTGRES_PORT: int
     POSTGRES_MAIN_DATABASE: str
     
-    model_config = SettingsConfigDict(    
+    model_config = SettingsConfigDict(
         env_file='.env',
         env_file_encoding='utf-8',
     )
+    
+    @property
+    def DATABASE_URL(self) -> str:
+        return (
+            f"postgresql://{self.POSTGRES_USERNAME}:{self.POSTGRES_PASSWORD}"
+            f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_MAIN_DATABASE}"
+        )
+    
+    @property
+    def SERVER_URL(self) -> str:
+        return (
+            f"postgresql://{self.POSTGRES_USERNAME}:{self.POSTGRES_PASSWORD}"
+            f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/postgres"
+        )
+
 
 @lru_cache()
 def get_settings() -> Settings:
